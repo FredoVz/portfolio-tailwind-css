39fea72753afd7bdc786792932e7477838b90527
FredoVz <dm.fredo018@gmail.com>
2026-05-29 13:08:39 +0700

    menambahkan fitur kirim contact form

--- src/js/script.js
+++ src/js/script.js
@@ -57,3 +57,32 @@ if (
 } else {
   darkToggle.checked = false;
 }
+
+//Function handle contact form
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbw40Q7I35RYZcZ5s1iwDFyPwjJVa1xMsqx-LIHb9dYo8ZGHkIxlxafAw7Li3EzPYpeGeQ/exec"; //YOUR_SCRIPT_URL
+const form = document.forms["fredo-contact-form"]; //YOUR_FORM_NAME
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //Ketika tombol submit diklik
+  //tampilkan tombol loading, hilangkan tombol kirim
+  btnLoading.classList.toggle("hidden");
+  btnKirim.classList.toggle("hidden");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => response.json())
+    .then((response) => {
+      //tampilkan tombol kirim, hilangkan tombol loading
+      btnLoading.classList.toggle("hidden");
+      btnKirim.classList.toggle("hidden");
+      //tampilkan alert
+      myAlert.classList.toggle("hidden");
+      //reset formnya
+      form.reset();
+      //console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
